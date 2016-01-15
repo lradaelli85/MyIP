9a3c6c248c4dc829883135a6e8ad975582c3cc3e
LucaRadaelli <l.radaelli85@gmail.com>
2016-01-15 12:47:52 +0100

    Create myip.sh

---
+++ myip.sh
@@ -0,0 +1,14 @@
+#!/bin/bash 
+#script to retrieve the public ip of your internet connection
+#Author: Luca Radaelli <lradaelli85@users.noreply.github.com>
+
+DIG=`which dig`
+if [ $? -eq 0 ]
+ then
+ HOST="myip.opendns.com"
+ DNS="resolver1.opendns.com"
+ $DIG +short $HOST @$DNS
+ else
+  echo "dig is not installed,or not in the PATH"
+  exit 1;
+ fi
