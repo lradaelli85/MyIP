93aa4fa5a9f690c574c89b7efc4cbf8973ee6b63
LucaRadaelli <l.radaelli85@gmail.com>
2017-06-16 15:07:25 +0200

    imporoved detection of no internet connection

--- myip.sh
+++ myip.sh
@@ -3,13 +3,17 @@
 #Author: Luca Radaelli <lradaelli85@users.noreply.github.com>
 
 DIG=`which dig`
-if [ $? -eq 0 ]
- then
- HOST="myip.opendns.com"
- DNS="resolver1.opendns.com"
- echo -n "Your public ip is: "
- $DIG +short $HOST @$DNS
- else
-  echo "dig is not installed,or not in the PATH"
-  exit 1;
- fi
+   if [ $? -eq 0 ]
+    then
+     HOST="myip.opendns.com"
+     DNS="resolver1.opendns.com"
+     OPTIONS="+short +time=3 +tries=1"
+     RES=`$DIG $OPTIONS $HOST @$DNS 2> /dev/null`
+     if [ -z $RES ]
+       then
+        echo "NoInternet"
+       else
+        echo $RES
+     fi
+   fi
+
